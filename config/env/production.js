7ef43918c5e6bbd9909f71cbfc42654ad35c57e0
Michael Botelho <saggiatoreblue@gmail.com>
2015-09-13 18:45:38 -0400

    preps for deployment

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/wsr',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://saggiatoredev:Antimatter12!@ds035428.mongolab.com:35428/wsr',
 	assets: {
 		lib: {
 			css: [
