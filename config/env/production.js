fc2353f4ea9b7dcb27f87e25d140f46a900f470f
Michael Botelho <saggiatoreblue@gmail.com>
2015-09-13 19:37:12 -0400

    another attempt at fixing heroku

--- config/env/production.js
+++ config/env/production.js
@@ -1,26 +1,10 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://saggiatoredev:Antimatter12!@ds035428.mongolab.com:35428/wsr',
-	assets: {
-		lib: {
-
-			js: [
-                'public/lib/jquery/dist/jquery.min.js',
-                'public/lib/angular/angular.js',
-                'public/lib/angular-resource/angular-resource.js',
-                'public/lib/angular-cookies/angular-cookies.js',
-                'public/lib/angular-animate/angular-animate.js',
-                'public/lib/angular-touch/angular-touch.js',
-                'public/lib/angular-sanitize/angular-sanitize.js',
-                'public/lib/angular-ui-router/release/angular-ui-router.js',
-                'public/lib/angular-ui-utils/ui-utils.js',
-                'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
-			]
-		},
-		css: 'public/modules/**/css/*.css',
-		js: 'public/dist/application.min.js'
-	},
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/wsr',
+    app: {
+        title: 'WSR - Production Environment'
+    },
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
 		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
